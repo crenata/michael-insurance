836c781daf36466a6f708ec48e9aad6ac582420c
Hafiizh Ghulam <hafiizh.ghulam@frisidea.com>
2023-07-05 19:55:32 +0700

    fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,163 +46,161 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body>
+<body class="hold-transition sidebar-mini layout-fixed">
     <div id="app">
-        <div class="hold-transition sidebar-mini layout-fixed">
-            <div class="wrapper">
-                <!-- Preloader -->
-                <div class="preloader flex-column justify-content-center align-items-center">
-                    <img class="animation__shake" src="{{ asset("dist/img/AdminLTELogo.png") }}" alt="AdminLTELogo" height="60" width="60">
-                </div>
-
-                <!-- Navbar -->
-                <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-                    <!-- Left navbar links -->
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                        </li>
-                    </ul>
-                    <!--Right navbar links-->
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item d-none d-sm-inline-block">
-                            <a href="home.php" class="nav-link">Help</a>
-                        </li>
-                        <li class="nav-item d-none d-sm-inline-block">
-                            <a href="#" class="nav-link">Profile</a>
-                        </li>
-                        <li class="nav-item d-none d-sm-inline-block">
-                            <a href="#" class="nav-link">Contact Us</a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /.navbar -->
-
-                <!-- Main Sidebar Container -->
-                <aside class="main-sidebar sidebar-dark-primary elevation-4">
-                    <!-- Brand Logo -->
-                    <a href="home.php" class="brand-link">
-                        <img src="{{ asset("dist/img/AdminLTELogo.png") }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                        <span class="brand-text font-weight-light">E-Insurance</span>
-                    </a>
-
-                    <!-- Sidebar -->
-                    <div class="sidebar">
-                        <!-- Sidebar user panel (optional) -->
-                        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                            <div class="image">
-                                <img src="{{ asset("dist/img/user2-160x160.jpg") }}" class="img-circle elevation-2" alt="User Image">
-                            </div>
-                            <div class="info">
-                                <a href="#" class="d-block">{{ auth()->user()->name }}</a>
-                            </div>
-                        </div>
-
-                        <!-- SidebarSearch Form -->
-                        <div class="form-inline">
-                            <div class="input-group" data-widget="sidebar-search">
-                                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                                <div class="input-group-append">
-                                    <button class="btn btn-sidebar">
-                                        <i class="fas fa-search fa-fw"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-
-                        @if(auth()->user()->type === "broker")
-                            @include("layouts.sidebar-broker")
-                        @elseif(auth()->user()->type === "underwriting")
-                            @include("layouts.sidebar-underwriting")
-                        @else
-                            @include("layouts.sidebar-policy")
-                        @endif
-                    </div>
-                    <!-- /.sidebar -->
-                </aside>
-
-                <!-- Content Wrapper. Contains page content -->
-                <div class="content-wrapper">
-                    <!-- Content Header (Page header) -->
-                    <div class="content-header">
-                        <div class="container-fluid">
-                            <div class="row mb-2">
-                                <div class="col-sm-6">
-                                    <h1 class="m-0">Welcome, {{ auth()->user()->name }}!</h1>
-                                </div><!-- /.col -->
-                                <div class="col-sm-6">
-                                    <ol class="breadcrumb float-sm-right">
-                                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                        <li class="breadcrumb-item active">Dashboard</li>
-                                    </ol>
-                                </div><!-- /.col -->
-                            </div><!-- /.row -->
-                        </div><!-- /.container-fluid -->
-                    </div>
-                    <!-- /.content-header -->
-
-                    <!-- Main content -->
-                    @yield('content')
-                    <!-- /.content -->
-                </div>
-                <!-- /.content-wrapper -->
-                <footer class="main-footer">
-                    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-                    All rights reserved.
-                    <div class="float-right d-none d-sm-inline-block">
-                        <b>Version</b> 3.2.0
-                    </div>
-                </footer>
-
-                <!-- Control Sidebar -->
-                <aside class="control-sidebar control-sidebar-dark">
-                    <!-- Control sidebar content goes here -->
-                </aside>
-                <!-- /.control-sidebar -->
+        <div class="wrapper">
+            <!-- Preloader -->
+            <div class="preloader flex-column justify-content-center align-items-center">
+                <img class="animation__shake" src="{{ asset("dist/img/AdminLTELogo.png") }}" alt="AdminLTELogo" height="60" width="60">
             </div>
 
-            <script src="{{ asset("plugins/jquery/jquery.min.js") }}"></script>
-            <script src="{{ asset("plugins/jquery-ui/jquery-ui.min.js") }}"></script>
-            <script>
-                $.widget.bridge('uibutton', $.ui.button)
-            </script>
-            <script src="{{ asset("plugins/sparklines/sparkline.js") }}"></script>
-            <script src="{{ asset("plugins/jqvmap/jquery.vmap.min.js") }}"></script>
-            <script src="{{ asset("plugins/jqvmap/maps/jquery.vmap.usa.js") }}"></script>
-            <script src="{{ asset("plugins/jquery-knob/jquery.knob.min.js") }}"></script>
-            <script src="{{ asset("plugins/moment/moment.min.js") }}"></script>
-            <script src="{{ asset("plugins/daterangepicker/daterangepicker.js") }}"></script>
-            <script src="{{ asset("plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js") }}"></script>
-            <script src="{{ asset("plugins/summernote/summernote-bs5.min.js") }}"></script>
-            <script src="{{ asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}"></script>
-            <script src="{{ asset("dist/js/adminlte.js") }}"></script>
-            <script src="{{ asset("dist/js/pages/dashboard.js") }}"></script>
-            <script src="{{ asset("plugins/sweetalert2/sweetalert2.min.js") }}"></script>
-            <script src="{{ asset("plugins/toastr/toastr.min.js") }}"></script>
-            <script>
-                $(function() {
-                    var Toast = Swal.mixin({
-                        toast: true,
-                        position: 'top-end',
-                        showConfirmButton: false,
-                        timer: 3000
-                    });
+            <!-- Navbar -->
+            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+                <!-- Left navbar links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    </li>
+                </ul>
+                <!--Right navbar links-->
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item d-none d-sm-inline-block">
+                        <a href="home.php" class="nav-link">Help</a>
+                    </li>
+                    <li class="nav-item d-none d-sm-inline-block">
+                        <a href="#" class="nav-link">Profile</a>
+                    </li>
+                    <li class="nav-item d-none d-sm-inline-block">
+                        <a href="#" class="nav-link">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.navbar -->
 
-                    $('.toastrDefaultSuccess').click(function() {
-                        toastr.success('Lorem ipsum dolor sit amet, consetetur sadipscing hehe.')
-                    });
-                    $('.toastrDefaultInfo').click(function() {
-                        toastr.info('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
-                    });
-                    $('.toastrDefaultError').click(function() {
-                        toastr.error('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
-                    });
-                    $('.toastrDefaultWarning').click(function() {
-                        toastr.warning('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
-                    });
-                });
-            </script>
+            <!-- Main Sidebar Container -->
+            <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                <!-- Brand Logo -->
+                <a href="home.php" class="brand-link">
+                    <img src="{{ asset("dist/img/AdminLTELogo.png") }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                    <span class="brand-text font-weight-light">E-Insurance</span>
+                </a>
+
+                <!-- Sidebar -->
+                <div class="sidebar">
+                    <!-- Sidebar user panel (optional) -->
+                    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                        <div class="image">
+                            <img src="{{ asset("dist/img/user2-160x160.jpg") }}" class="img-circle elevation-2" alt="User Image">
+                        </div>
+                        <div class="info">
+                            <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+                        </div>
+                    </div>
+
+                    <!-- SidebarSearch Form -->
+                    <div class="form-inline">
+                        <div class="input-group" data-widget="sidebar-search">
+                            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                            <div class="input-group-append">
+                                <button class="btn btn-sidebar">
+                                    <i class="fas fa-search fa-fw"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    @if(auth()->user()->type === "broker")
+                        @include("layouts.sidebar-broker")
+                    @elseif(auth()->user()->type === "underwriting")
+                        @include("layouts.sidebar-underwriting")
+                    @else
+                        @include("layouts.sidebar-policy")
+                    @endif
+                </div>
+                <!-- /.sidebar -->
+            </aside>
+
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0">Welcome, {{ auth()->user()->name }}!</h1>
+                            </div><!-- /.col -->
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item active">Dashboard</li>
+                                </ol>
+                            </div><!-- /.col -->
+                        </div><!-- /.row -->
+                    </div><!-- /.container-fluid -->
+                </div>
+                <!-- /.content-header -->
+
+                <!-- Main content -->
+            @yield('content')
+            <!-- /.content -->
+            </div>
+            <!-- /.content-wrapper -->
+            <footer class="main-footer">
+                <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+                All rights reserved.
+                <div class="float-right d-none d-sm-inline-block">
+                    <b>Version</b> 3.2.0
+                </div>
+            </footer>
+
+            <!-- Control Sidebar -->
+            <aside class="control-sidebar control-sidebar-dark">
+                <!-- Control sidebar content goes here -->
+            </aside>
+            <!-- /.control-sidebar -->
         </div>
+
+        <script src="{{ asset("plugins/jquery/jquery.min.js") }}"></script>
+        <script src="{{ asset("plugins/jquery-ui/jquery-ui.min.js") }}"></script>
+        <script>
+            $.widget.bridge('uibutton', $.ui.button)
+        </script>
+        <script src="{{ asset("plugins/sparklines/sparkline.js") }}"></script>
+        <script src="{{ asset("plugins/jqvmap/jquery.vmap.min.js") }}"></script>
+        <script src="{{ asset("plugins/jqvmap/maps/jquery.vmap.usa.js") }}"></script>
+        <script src="{{ asset("plugins/jquery-knob/jquery.knob.min.js") }}"></script>
+        <script src="{{ asset("plugins/moment/moment.min.js") }}"></script>
+        <script src="{{ asset("plugins/daterangepicker/daterangepicker.js") }}"></script>
+        <script src="{{ asset("plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js") }}"></script>
+        <script src="{{ asset("plugins/summernote/summernote-bs5.min.js") }}"></script>
+        <script src="{{ asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}"></script>
+        <script src="{{ asset("dist/js/adminlte.js") }}"></script>
+        <script src="{{ asset("dist/js/pages/dashboard.js") }}"></script>
+        <script src="{{ asset("plugins/sweetalert2/sweetalert2.min.js") }}"></script>
+        <script src="{{ asset("plugins/toastr/toastr.min.js") }}"></script>
+        <script>
+            $(function() {
+                var Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+
+                $('.toastrDefaultSuccess').click(function() {
+                    toastr.success('Lorem ipsum dolor sit amet, consetetur sadipscing hehe.')
+                });
+                $('.toastrDefaultInfo').click(function() {
+                    toastr.info('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
+                });
+                $('.toastrDefaultError').click(function() {
+                    toastr.error('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
+                });
+                $('.toastrDefaultWarning').click(function() {
+                    toastr.warning('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
+                });
+            });
+        </script>
     </div>
 </body>
 </html>
